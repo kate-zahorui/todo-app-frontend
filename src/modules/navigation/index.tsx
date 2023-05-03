fe9af1367b1b72886342987c88fb4abd031b4344
kate-zahorui <kate.zahorui@gmail.com>
2023-05-03 11:33:23 +0300

    Fixes router

--- src/modules/navigation/index.tsx
+++ src/modules/navigation/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AddOrUpdateTodoPage, HomePageContainer } from '../pages';
 import { APP_KEYS } from '../common/consts';
 
 export const MainRouter = () => (
   <Router basename="/todo-app-frontend">
     <Routes>
-      <Route element={<HomePageContainer />} path={APP_KEYS.ROUTER_KEYS.HOME} />
-      <Route element={<AddOrUpdateTodoPage />} path={APP_KEYS.ROUTER_KEYS.ADD_TODO} />
-      <Route element={<AddOrUpdateTodoPage />} path={APP_KEYS.ROUTER_KEYS.TODO_DETAILS} />
+      <Route
+        path={APP_KEYS.ROUTER_KEYS.ROOT}
+        element={<Navigate to={APP_KEYS.ROUTER_KEYS.HOME} />}
+      />
+      <Route path={APP_KEYS.ROUTER_KEYS.HOME} element={<HomePageContainer />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.ADD_TODO} element={<AddOrUpdateTodoPage />} />
+      <Route path={APP_KEYS.ROUTER_KEYS.TODO_DETAILS} element={<AddOrUpdateTodoPage />} />
+      <Route path="*" element={<Navigate to={APP_KEYS.ROUTER_KEYS.HOME} />} />
     </Routes>
   </Router>
 );
